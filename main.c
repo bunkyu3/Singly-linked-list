6e521f1bbd543cbff6d757d81a3109bdbc679632
aki <bunkyu3>
2020-05-02 17:58:21 +0900

    Step2.4: 関数を一つにまとめる

--- main.c
+++ main.c
@@ -3,9 +3,7 @@
 #include "define.h" 
 #include "func.h"
 
-void InsertToHead(struct ListNode **head_p, int data);
-void InsertToTail(struct ListNode **head_p, int data);
-void InsertToMiddle(struct ListNode **head_p, int data, int position);
+void InsertInLinkedList(struct ListNode **head_p, int data, int position);
 
 int main(void){
 	struct ListNode five = {5, NULL};
@@ -17,47 +15,32 @@ int main(void){
 	printf("before\n");
 	PrintList(head);
 
-	InsertToHead(head_p, 2);
-	InsertToTail(head_p, 10);
-	InsertToMiddle(head_p, 7, 3);
+	InsertInLinkedList(head_p, 2, 1);
+	InsertInLinkedList(head_p, 10, 3);
+	InsertInLinkedList(head_p, 7, 3);
 
 	printf("after\n");
 	PrintList(head);
 	return 0;
 }
 
-void InsertToHead(struct ListNode **head_p, int data){
-	struct ListNode *newNode;
-	newNode = (struct ListNode *)malloc(sizeof(struct ListNode));
-	newNode->data = data;
-	newNode->next = *head_p;
-	*head_p = newNode;
-}
-
-void InsertToTail(struct ListNode **head_p, int data){
+void InsertInLinkedList(struct ListNode **head_p, int data, int position){
+	int i=1;
 	struct ListNode *newNode, *p, *q;
 	newNode = (struct ListNode *)malloc(sizeof(struct ListNode));
 	newNode->data = data;
 	p = *head_p;
-	while(p != NULL){
-		q = p;
-		p = p->next;
+	if(position == 1){
+		newNode->next = p;
+		*head_p = newNode;
+	}else{
+		while( (p != NULL)&&(i<position) ){
+			i++;
+			q = p;
+			p = p->next;
+		}
+		q->next = newNode;
+		newNode->next = p;
 	}
-	q->next = newNode;
-	newNode->next = p;
-}
 
-void InsertToMiddle(struct ListNode **head_p, int data, int position){
-	int i = 1;
-	struct ListNode *newNode, *p, *q;
-	newNode = (struct ListNode *)malloc(sizeof(struct ListNode));
-	newNode->data = data;
-	p = *head_p;
-	while(i<position){
-		i++;
-		q = p;
-		p = p->next;
-	}
-	q->next = newNode;
-	newNode->next = p;
 }
